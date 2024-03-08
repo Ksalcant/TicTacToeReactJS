42a1d4d8ea0c013b089b737c7dc30db2008f95ac
Your GitHub Username <Your GitHub Email>
2024-03-07 19:53:17 -0500

    Added the board

--- TicTacToeGame/src/App.jsx
+++ TicTacToeGame/src/App.jsx
@@ -76,3 +76,54 @@ export default function Game() {
     </div>
   );
 }
+
+function Board({ xIsNext, squares, onPlay }) {
+  // const [xIsNext, setXIsNext] = useState(true);
+  // global state variable
+  //const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+  // define the onSquare function before using import
+  // this create a copy of the squares array
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      // if the square is set to true, return
+      return;
+    }
+    // copy the squares array
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+    // setSquares(nextSquares);
+    // setXIsNext(!xIsNext);
+  }
+  return (
+    <>
+      <div className="status"> {status} </div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
