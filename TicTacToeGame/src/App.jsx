9702b23dff3547aba2055853cac7a87f86c6e098
Your GitHub Username <Your GitHub Email>
2024-03-07 19:51:58 -0500

    Added the game component to the App file

--- TicTacToeGame/src/App.jsx
+++ TicTacToeGame/src/App.jsx
@@ -32,4 +32,47 @@ function App() {
   )
 }
 
-export default App
+// Create a top level component called game to manage the board
+export default function Game() {
+  // add state variables to track whose turn is next
+  const [xIsNext, setXIsNext] = useState(true);
+  // array of history moves in state
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // create a new array that contains all the items in history followed by nextSquares
+    // this merges all the arrays into groups in only 1 array.
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  // Transform history of moves into react elements representing buttons on the screen
+  function jumpTo(nextMove) {
+    // TODO
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      // list of items tha contains buttons
+      <li>
+        {/*onClick call the function jumpTo */}
+        <button onClick={() => jumpTo(move)}> {description} </button>
+      </li>
+    );
+  });
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol> {moves} </ol>
+      </div>
+    </div>
+  );
+}
