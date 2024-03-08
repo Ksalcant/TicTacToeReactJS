b9b271dd82dfa257140eb195f1bf9c8b053399a6
Your GitHub Username <Your GitHub Email>
2024-03-07 19:55:36 -0500

    Added the square component

--- TicTacToeGame/src/App.jsx
+++ TicTacToeGame/src/App.jsx
@@ -3,34 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
 // Create a top level component called game to manage the board
 export default function Game() {
@@ -125,5 +98,19 @@ function Board({ xIsNext, squares, onPlay }) {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+function Square({ value, onSquareClick }) {
+  // set state of each click
+  //const [value, setValue] = useState(null)
+  // function handleClick(){
+  //   //console.log('clicked!');
+  //   setValue(value);
+  // }
+  return (
+    <button className="Square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
